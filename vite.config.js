625b195e14675924b2f519b610dd76e4ceadca57
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-10-06 18:07:13 +0100

    added config for absolute import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from "fs"
+
+const folders = fs.readdirSync('./src', { withFileTypes: true });
+
+const fileNames = folders.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
+
+const filePaths = fileNames.reduce(
+    (acc, cur) => ({
+      ...acc,
+      [cur]: `/${cur === 'src' ? cur : 'src/' + cur}`
+    }),
+    ''
+  );
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      ...filePaths
+    }
+  }
+  
 })
