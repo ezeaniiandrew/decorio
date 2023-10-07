64fac5a35791d39425598bc62da284262050ae61
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-10-07 16:08:17 +0100

    changed the names of some images and deleted unnecessary images

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from "fs"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-const folders = fs.readdirSync('./src', { withFileTypes: true });
+const folders = fs.readdirSync("./src", { withFileTypes: true });
 
-const fileNames = folders.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
+const fileNames = folders
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
 
 const filePaths = fileNames.reduce(
-    (acc, cur) => ({
-      ...acc,
-      [cur]: `/${cur === 'src' ? cur : 'src/' + cur}`
-    }),
-    ''
-  );
+  (acc, cur) => ({
+    ...acc,
+    [cur]: `/${cur === "src" ? cur : "src/" + cur}`,
+  }),
+  ""
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      ...filePaths
-    }
-  }
-  
-})
+      ...filePaths,
+    },
+  },
+});
