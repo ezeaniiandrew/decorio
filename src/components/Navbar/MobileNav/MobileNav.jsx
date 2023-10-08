f23a3e1ae822f1f44508ec5671e8388279a7ca38
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-10-08 13:22:54 +0100

    made mobile navigation functional

--- src/components/Navbar/MobileNav/MobileNav.jsx
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -1,24 +1,43 @@
 import c from "./mobile-nav.module.css";
 import headerLogo from "assets/header-logo.png";
-import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { TbShoppingCartFilled } from "react-icons/tb";
+import { IconContext } from "react-icons";
+import { useState } from "react";
+import NavLink from "components/NavLink/NavLink";
+
+const links = ["home", "explore", "about", "support", "product comparison"];
 
 function MobileNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <div className={c.logoBox}>
         <img src={headerLogo} alt="company logo" />
       </div>
       <div className={c.iconBox}>
-        <div className="icon">
-          <AiOutlineSearch />
-        </div>
-        <div className="icon">
-          <AiOutlineShoppingCart />
-        </div>
+        <IconContext.Provider value={{ size: "1.5rem" }}>
+          <div>
+            <AiOutlineSearch />
+          </div>
+          <div>
+            <TbShoppingCartFilled />
+          </div>
+          <div className={c.menuIcon} onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+          </div>
+        </IconContext.Provider>
       </div>
-      <div className="icon">
-        <GiHamburgerMenu />
+      <div className={`${!isOpen ? c.blur + c.show : c.blur}`}>
+        <nav className={c.nav}>
+          <ul className={c.navLinks}>
+            {links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </ul>
+        </nav>
       </div>
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,31 @@
+import MobileNav from "./MobileNav/MobileNav";
 import c from "./navbar.module.css";
-import MobileNav from "components/MobileNav/MobileNav";
+import { useState, useEffect } from "react";
+
+const mediaQuery = window.matchMedia("(max-width: 600px)");
 
 function Navbar() {
+  const [isMobile, setIsMobile] = useState(mediaQuery.matches);
+
+  useEffect(() => {
+    const handleMediaQueryChange = (e) => setIsMobile(e.matches);
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
-    <header className={c.header}>
-      <MobileNav />
-    </header>
+    <>
+      {isMobile ? (
+        <header className={c.header}>
+          <MobileNav />
+        </header>
+      ) : (
+        <p>Hello</p>
+      )}
+    </>
   );
 }
 
