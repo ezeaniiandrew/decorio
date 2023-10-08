887700d7b985e7252c92f03a27894bf77ed934aa
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-10-08 21:22:06 +0100

    improved the responsiveness of the mobile menu

--- src/components/Navbar/MobileNav/MobileNav.jsx
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -3,41 +3,43 @@ import headerLogo from "assets/header-logo.png";
 import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { TbShoppingCartFilled } from "react-icons/tb";
-import { IconContext } from "react-icons";
 import { useState } from "react";
 import NavLink from "components/NavLink/NavLink";
-
-const links = ["home", "explore", "about", "support", "product comparison"];
+import CustomizedIcon from "components/CustomizedIcon/CustomizedIcon";
+import { navLinks } from "constants";
 
 function MobileNav() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <div className={c.logoBox}>
-        <img src={headerLogo} alt="company logo" />
-      </div>
-      <div className={c.iconBox}>
-        <IconContext.Provider value={{ size: "1.5rem" }}>
-          <div>
+      <div className={c.menu}>
+        <div className={c.logoBox}>
+          <img src={headerLogo} alt="company logo" />
+        </div>
+        <div className={c.iconBox}>
+          <CustomizedIcon>
             <AiOutlineSearch />
-          </div>
-          <div>
+          </CustomizedIcon>
+          <CustomizedIcon>
             <TbShoppingCartFilled />
-          </div>
-          <div className={c.menuIcon} onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
-          </div>
-        </IconContext.Provider>
+          </CustomizedIcon>
+        </div>
       </div>
-      <div className={`${!isOpen ? c.blur + c.show : c.blur}`}>
-        <nav className={c.nav}>
-          <ul className={c.navLinks}>
-            {links.map((link) => (
+
+      <div className={c.navbar}>
+        <nav className={`${isOpen ? c.active : ""}`}>
+          <ul>
+            {navLinks.map((link) => (
               <NavLink key={link}>{link}</NavLink>
             ))}
           </ul>
         </nav>
+        <div onClick={() => setIsOpen(!isOpen)}>
+          <CustomizedIcon>
+            {isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+          </CustomizedIcon>
+        </div>
       </div>
     </>
   );
