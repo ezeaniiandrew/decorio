ea0dac9c178568c620cb27d937f68ff1c1d8bc99
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-10-11 17:43:38 +0100

    create a hero component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import c from "./header.module.css";
+// import heroImage from "assets/hero-img.jpg";
+
+function Header() {
+  return (
+    <section className={c.hero}>
+      {/* <div className={c.heroImage}>
+        <img src={heroImage} alt="" />
+      </div> */}
+    </section>
+  );
+}
+
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Header from "components/Header/Header";
 import Navbar from "components/Navbar/Navbar";
 
 function Home() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   );
 }
