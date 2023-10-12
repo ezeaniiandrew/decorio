a89646f2461b6fa66893db2a15b16c614d3dfede
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-10-12 22:31:58 +0100

    added Hero component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
-import Header from "components/Header/Header";
+import Button from "components/Button/Button";
+import Hero from "components/Hero/Hero";
 import Navbar from "components/Navbar/Navbar";
+import SectionCard from "components/SectionCard/SectionCard";
+import SectionHeader from "components/SectionHeader/SectionHeader";
 
 function Home() {
   return (
     <>
       <Navbar />
-      <Header />
+      <Hero />
+      <SectionCard>
+        <SectionHeader
+          title="shop from collections"
+          actionElement={
+            <Button variant="text" pd="4">
+              view more
+            </Button>
+          }
+        />
+      </SectionCard>
     </>
   );
 }
