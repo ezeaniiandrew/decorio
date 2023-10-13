c6da0aa50a462f3d7926cd60d422468a4970a2ac
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-10-13 21:38:15 +0100

    added collections data

--- src/data/index.jsx
+++ src/data/index.jsx
@@ -1,7 +1,29 @@
+import img1 from "assets/chair-five.png";
+import img2 from "assets/chair-six.png";
+import img3 from "assets/brown-wooden-chair.jpg";
+
 export const navLinks = [
   "home",
   "explore",
   "about",
   "support",
   "product comparison",
+];
+
+export const collectionImages = [
+  {
+    img: img1,
+    category: "Living Room",
+    qty_left: 130,
+  },
+  {
+    img: img2,
+    category: "Living Room",
+    qty_left: 130,
+  },
+  {
+    img: img3,
+    category: "Living Room",
+    qty_left: 130,
+  },
 ];
